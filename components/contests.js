eb427f42cffe70ee7380a60d80a5fa6db6c10c32
arun1705 <arun.hosamani@nelito.com>
2019-03-06 17:06:21 +0530

    entry fee added

--- components/contests.js
+++ components/contests.js
@@ -13,9 +13,17 @@ import {
   Alert,
 } from 'react-native';
 import ProgressBarAnimated from 'react-native-progress-bar-animated';
+
 const barWidth = Dimensions.get('screen').width - 120;
+const progressCustomStyles = {
+  backgroundColor: 'red',
+  borderRadius: 0,
+  height: 4,
+  borderColor: 'black',
+};
 const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-export class Contests extends Component {
+
+class Contest extends Component {
   state = {
     progressCustomized: 1,
   };
@@ -26,34 +34,29 @@ export class Contests extends Component {
     });
   };
 
-  // renderRow = (rowData, sectionID, rowID) => (
-  //   <TouchableOpacity style={styles.buttonContents} onPress={this.onPress}>
-  //     <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-  //       <Image source={require('../assets/team1.png')} />
-  //       <Text style={styles.subTitle}>{rowData.subtitle}</Text>
-  //       <Image source={require('../assets/team2.png')} />
-  //     </View>
-  //   </TouchableOpacity>
-  // );
-
   render() {
     return (
-      // <ListView
-      //   style={styles.listview}
-      //   dataSource={this.state.dataSource}
-      //   renderRow={this.renderRow}
-      // />
       <View style={styles.container}>
-        <View>
-          <Text style={styles.label}>Custom style with max value in 30%</Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
           <ProgressBarAnimated
-            style={styles.progressCustomStyles}
+            {...progressCustomStyles}
             width={barWidth}
-            maxValue={300}
+            maxValue={600}
             value={this.state.progressCustomized}
           />
           <View style={styles.buttonContainer}>
-            <View style={styles.buttonInner}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}>
+              <Text style={{ color: '#000000' }}>"$ 40"</Text>
               <Button
                 title="join"
                 onPress={this.increase.bind(this, 'progressCustomized', 1)}
@@ -70,24 +73,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF',
-    marginTop: 50,
+    marginTop: 20,
     padding: 15,
   },
   buttonContainer: {
     marginTop: 15,
-    width: '20%',
-    height: '8%',
-  },
-  label: {
-    color: '#999',
-    fontSize: 14,
-    fontWeight: '500',
-    marginBottom: 10,
-  },
-  progressCustomStyles: {
-    backgroundColor: 'red',
-    borderRadius: 0.5,
-    height: 4,
-    borderColor: 'grey',
+    width: '30%',
+    height: '10%',
+    borderColor: 'orange',
   },
 });
+
+export default Contest;
